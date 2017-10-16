c929b9ee2c97f4768c71f8f57c7d35b2e18a23d9
lmotta <motta.luiz@gmail.com>
2017-10-16 10:06:35 -0200

    Create run-qgis3-docker.sh

---
+++ run-qgis3-docker.sh
@@ -0,0 +1,12 @@
+# Adaptation from https://hub.docker.com/r/kartoza/qgis-desktop/
+xhost +
+# Users home is mounted as home
+# --rm will remove the container as soon as it ends
+
+docker run --rm --name="qgis3" \
+    -i -t \
+    -v ${HOME}:/home/${USER} \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e DISPLAY=unix$DISPLAY \
+    qgis3
+xhost -
